014f056e65a5c0a062c2910ca078b65e857f7ae8
Jeoh Garche <jeohgarche@gmail.com>
2022-05-30 17:17:33 +0800

    Updates

---
+++ precache-manifest.b02e694457cd3aef4ef7b7dbca6cb215.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "253c2d48ce9f28c55132",
+    "url": "/Jeoh_React/static/css/main.19cb8c5f.chunk.css"
+  },
+  {
+    "revision": "253c2d48ce9f28c55132",
+    "url": "/Jeoh_React/static/js/main.87f56adc.chunk.js"
+  },
+  {
+    "revision": "11d0563e64fa6a1521c9",
+    "url": "/Jeoh_React/static/js/runtime~main.fc5c259b.js"
+  },
+  {
+    "revision": "0d300dd868c5272e826a",
+    "url": "/Jeoh_React/static/css/2.0ca95a0c.chunk.css"
+  },
+  {
+    "revision": "0d300dd868c5272e826a",
+    "url": "/Jeoh_React/static/js/2.738473dc.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/Jeoh_React/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "b8992aea52845a2523757279c21f5483",
+    "url": "/Jeoh_React/static/media/hero-bg.b8992aea.jpg"
+  },
+  {
+    "revision": "9be61c34d688ea3323cfcba99e4459ed",
+    "url": "/Jeoh_React/index.html"
+  }
+];
