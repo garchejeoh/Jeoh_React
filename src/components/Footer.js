49c943fc5d3a98ca2ddd3921c5c874174304efb6
Jeoh Garche <jeohgarche@gmail.com>
2022-05-30 13:24:56 +0800

    Demmo commit

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+        <footer id="footer">
+        <div class="footer-top">
+          <div class="container">
+    
+          </div>
+        </div>
+    
+        <div class="container">
+          <div class="copyright">
+            &copy; Copyright <strong>Jeoh Garche IT2C</strong>. All Rights Reserved
+          </div>
+        </div>
+      </footer>
+      
+    );
+  }
+}
+export default Footer;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+import App from './App';
+import 'node_module/bootstrap/dist/css/bootstrap.min.css';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+reportWebVitals();
